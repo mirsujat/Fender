1fe0c2855daf32d118414b5a2ceab0f9f782db23
Mir Sujat <mir.sujat@gmail.com>
2020-06-28 16:56:23 +0600

    Create Card.js

---
+++ easy/src/components/Card/Card.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import "./styles.css";
+
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1,
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+const Card = () => {
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+  return (
+    <div className="product-card">
+      <animated.div
+        class="card"
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        style={{ transform: props.xys.interpolate(trans) }}
+      />
+      <div className="product-card-footer">
+        <div className="name">Product Name</div>
+        <div className="price">$ 45</div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
