088559bb0234808ba78f89c506ea249986ba0ece
Mir Sujat <mir.sujat@gmail.com>
2020-06-28 16:56:58 +0600

    Create ProductPreview.js

---
+++ easy/src/components/ProductPreview/ProductPreview.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "../Card/Card";
+
+import "./styles.css";
+
+const ProductPreview = () => (
+  <div className="product-preview">
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+  </div>
+);
+
+export default ProductPreview;
