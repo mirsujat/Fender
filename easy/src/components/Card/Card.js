73b33dd0e1310fd8a4ac8e82d6c6ba0410f7dc30
Mir Sujat <mir.sujat@gmail.com>
2020-06-29 12:43:58 +0600

    Update Card.js

--- easy/src/components/Card/Card.js
+++ easy/src/components/Card/Card.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
-import "./styles.css";
+import CustomButton from "../CustomButton/CustomButton";
+import "./card.css";
 
 const calc = (x, y) => [
   -(y - window.innerHeight / 2) / 20,
@@ -11,6 +12,13 @@ const trans = (x, y, s) =>
   `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 const Card = () => {
+  const [state, toggle] = useState(true);
+  const { x } = useSpring({
+    from: { x: 0 },
+    x: state ? 1 : 0,
+    config: { duration: 1000 },
+  });
+
   const [props, set] = useSpring(() => ({
     xys: [0, 0, 1],
     config: { mass: 5, tension: 350, friction: 40 },
@@ -22,10 +30,26 @@ const Card = () => {
         onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
         onMouseLeave={() => set({ xys: [0, 0, 1] })}
         style={{ transform: props.xys.interpolate(trans) }}
-      />
-      <div className="product-card-footer">
-        <div className="name">Product Name</div>
-        <div className="price">$ 45</div>
+      >
+        <div className="product-card-footer">
+          <div className="name">Product Name</div>
+          <div className="price">$ 45</div>
+        </div>
+      </animated.div>
+      <div className="buy-now" onClick={() => toggle(!state)}>
+        <animated.div
+          style={{
+            opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+            transform: x
+              .interpolate({
+                range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+              })
+              .interpolate((x) => `scale(${x})`),
+          }}
+        >
+          <CustomButton>BUY NOW</CustomButton>
+        </animated.div>
       </div>
     </div>
   );
