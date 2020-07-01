2c01e6ba3d8259ea5aef6b71f9f16a125d89e24d
Mir Sujat <mir.sujat@gmail.com>
2020-07-01 11:20:36 +0600

    Update Navbar.js

--- easy/src/components/Navbar/Navbar.js
+++ easy/src/components/Navbar/Navbar.js
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => (
-  <div className="nav-bar">
-    <ul className="nav">
-      <li className="nav-list">
-        <Link to="/">Simple</Link>
-      </li>
-      <li className="nav-list">
-        <Link to="/basic">Basic</Link>
-      </li>
-      <li className="nav-list">
-        <Link to="/custom">Custom</Link>
-      </li>
-      <li className="nav-list">
-        <Link to="/advance">Advance</Link>
-      </li>
-      <li className="nav-list">
-        <Link to="/spring">Spring Animation</Link>
-      </li>
-    </ul>
-  </div>
-);
+import Bar from "../Bar/Bar";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav-bar">
+      <div className="nav-right">
+        <Bar></Bar>
+      </div>
+      <ul className="nav nav-left">
+        <li className="nav-list">
+          <Link to="/">Simple</Link>
+        </li>
+        <li className="nav-list">
+          <Link to="/basic">Basic</Link>
+        </li>
+        <li className="nav-list">
+          <Link to="/custom">Custom</Link>
+        </li>
+        <li className="nav-list">
+          <Link to="/advance">Advance</Link>
+        </li>
+        <li className="nav-list">
+          <Link to="/spring">Spring Animation</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
