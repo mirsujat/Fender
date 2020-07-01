fd80c798dd75facbf8a6e961a04ac9df7123e1ef
Mir Sujat <mir.sujat@gmail.com>
2020-07-01 11:20:13 +0600

    Update Card.js

--- easy/src/components/Card/Card.js
+++ easy/src/components/Card/Card.js
@@ -23,6 +23,17 @@ const Card = () => {
     xys: [0, 0, 1],
     config: { mass: 5, tension: 350, friction: 40 },
   }));
+
+  let animClass = {
+    opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+    transform: x
+      .interpolate({
+        range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+        output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+      })
+      .interpolate((x) => `scale(${x})`),
+  };
+
   return (
     <div className="product-card">
       <animated.div
@@ -37,17 +48,7 @@ const Card = () => {
         </div>
       </animated.div>
       <div className="buy-now" onClick={() => toggle(!state)}>
-        <animated.div
-          style={{
-            opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
-            transform: x
-              .interpolate({
-                range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-                output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
-              })
-              .interpolate((x) => `scale(${x})`),
-          }}
-        >
+        <animated.div style={animClass}>
           <CustomButton>BUY NOW</CustomButton>
         </animated.div>
       </div>
